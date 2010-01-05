74833acd678bf7b1442d349d33aa46d700ddce7d
JTee <JTee@jtee-mbp-osx.fritz.box>
2010-01-05 20:55:18 +0100

    master acho added

--- mixAudio.c
+++ mixAudio.c
@@ -1,5 +1,7 @@
 #include "mixAudio.h"
 
+#define echoLen 10000
+
 int range = 32;
 
 void mixAudio(void *unused, Uint8 *stream, int len) {
@@ -8,11 +10,24 @@ void mixAudio(void *unused, Uint8 *stream, int len) {
 
 	for (i=0;i<len;i++) {
 
-		outputValue = sumVoices() * range;
+		outputValue = fxEcho(compVoices()) * range;
 
 		if (outputValue > 127) outputValue = 127;        /* and clip the result */
 		if (outputValue < -128) outputValue = -128;      /* this seems a crude method, but works very well */
 
 		stream[i] = outputValue;
 	}
+}
+
+float fxEcho(float in) {
+	static float preVals[echoLen];
+	static float *prePoint = preVals;
+	float out = 0.7 * in + 0.3 * *prePoint;
+
+	*prePoint = in;
+
+	prePoint++;
+	if (prePoint == preVals + echoLen) prePoint = preVals;
+		
+	return out;
 }
--- mixAudio.h
+++ mixAudio.h
@@ -2,3 +2,5 @@
 #include <SDL.h>
 
 void mixAudio(void *unused, Uint8 *stream, int len);
+
+float fxEcho(float);
