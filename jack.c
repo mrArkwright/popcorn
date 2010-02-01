24991f4a81992dae03bd2243a8cf5fe2d196f2cf
cryptix <cryptix@riseup.net>
2010-02-01 12:56:59 +0100

    jack.c - debug off

--- jack.c
+++ jack.c
@@ -22,7 +22,7 @@ int jackProcess(jack_nframes_t nframes, void *arg) {
 
 	midiEventCount = jack_midi_get_event_count(midiBuf);
 
-	/* print events to stdout */
+	/* print events to stdout 
 	if(midiEventCount >= 1) {
 		printf(" popcorn: have %d events \n", midiEventCount);
 		for(i=0; i<midiEventCount; i++) {
@@ -35,6 +35,7 @@ int jackProcess(jack_nframes_t nframes, void *arg) {
 		}
 		printf("\n\n");
 	}
+	*/
 
 /* TODO: - cache midi events */	
 	
@@ -44,14 +45,14 @@ int jackProcess(jack_nframes_t nframes, void *arg) {
 		for(midiEventIndex = 0; midiEventIndex < midiEventCount; midiEventIndex++) {
 			jack_midi_event_get(&midiEvent, midiBuf, midiEventIndex);
 			if(midiEvent.time == i) {
-				printf("Handling event %d. time is %d.", 0, midiEvent.time);
+				/*printf("Handling event %d. time is %d.", 0, midiEvent.time);*/
 				/* play / stop note */
 				if( (*(midiEvent.buffer) & 0xf0) == 0x90) {
-					printf(" starting note: 0x%x\n", *(midiEvent.buffer+1));
+					/*printf(" starting note: 0x%x\n", *(midiEvent.buffer+1));*/
 					startVoice( *(midiEvent.buffer + 1), *(midiEvent.buffer + 2));
 				}
 				else if( (*(midiEvent.buffer) & 0xf0) == 0x80) {
-					printf(" stoping note: 0x%x\n", *(midiEvent.buffer+1));
+					/*printf(" stoping note: 0x%x\n", *(midiEvent.buffer+1));*/
 					stopVoice( *(midiEvent.buffer + 1), *(midiEvent.buffer + 2));
 				}
 			}
